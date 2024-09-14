542bc61ebd09af2dfa52bf68a88d51af7c8d6079
paulvinueza30 <paulvinueza30@gmail.com>
2024-09-14 17:21:22 -0400

    added About component

--- shawarma-brothers/src/app/page.tsx
+++ shawarma-brothers/src/app/page.tsx
@@ -1,15 +1,16 @@
 // Update the import statements
 import { ContactForm } from "@/components/ContactForm";
 import { Location } from "@/components/Location";
-import ShawarmaHero from '@/components/ShawarmaHero'
-import MenuGrid from '@/components/MenuGrid'
-
+import { About } from "@/components/About";
+import ShawarmaHero from "@/components/ShawarmaHero";
+import MenuGrid from "@/components/MenuGrid";
 
 export default function Home() {
   return (
     <main>
       <ShawarmaHero />
       <MenuGrid />
+      <About />
       <div className="flex flex-col items-center justify-center min-h-screen p-8">
         <Location />
         <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
